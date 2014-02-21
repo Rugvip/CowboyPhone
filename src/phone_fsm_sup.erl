15fe95f789cc20ccca5b1e2abe55652999482814
Patrik Oldsberg <poldsberg@gmail.com>
2014-02-21 01:02:21 +0100

    be more brutal when stopping phone_fsm_sup, and more kind when killing it's children

--- src/phone_fsm_sup.erl
+++ src/phone_fsm_sup.erl
@@ -9,15 +9,15 @@ init([]) ->
     Fsm = {0, {?FSM, start_link, []}, temporary, 5000, worker, [?FSM]},
     {ok, {{simple_one_for_one, 10, 10}, [Fsm]}}.
 
-stop(Pid) -> exit(Pid, normal), ok.
+stop(Pid) -> exit(Pid, kill), ok.
 
 add_controller(SupPid, Number) ->
     {ok, Pid} = supervisor:start_child(SupPid,  [Number]),
     {ok, Pid}.
 
 remove_controller(SupPid, Number) ->
-    {ok, Pid} = hlr:lookup_id(Number),
-    % exit(Pid, normal),
-    ok = supervisor:terminate_child(SupPid, Pid),
-    io:format("terminating ~p: ~p~n", [Pid, Number]),
+    case hlr:lookup_id(Number) of
+        {ok, Pid} -> ok = supervisor:terminate_child(SupPid, Pid);
+        _ -> ok
+    end,
     ok.
